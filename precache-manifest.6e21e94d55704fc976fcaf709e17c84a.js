5031d382f9822d5ad8e3ac7a0c1a72109ac7320d
Mat Wilmot <matwilmot90@gmail.com>
2020-08-24 15:51:09 -0700

    Updates

---
+++ precache-manifest.6e21e94d55704fc976fcaf709e17c84a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71bec1385f3cbdb4bc584a78ea59ca12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3bd7eac1b0a03e36aa2",
+    "url": "/static/css/main.85ae41ea.chunk.css"
+  },
+  {
+    "revision": "14a73041c98f32714987",
+    "url": "/static/js/2.79a2e5a1.chunk.js"
+  },
+  {
+    "revision": "f25112c0081db71b0e5dd2ce64e909e6",
+    "url": "/static/js/2.79a2e5a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3bd7eac1b0a03e36aa2",
+    "url": "/static/js/main.b652f378.chunk.js"
+  },
+  {
+    "revision": "5ae00f89d0a748566589",
+    "url": "/static/js/runtime-main.9cc53fc4.js"
+  },
+  {
+    "revision": "37a401aea4e6c05175d2f9a6d46f072d",
+    "url": "/static/media/20190730_145147.37a401ae.jpg"
+  },
+  {
+    "revision": "ec79523b1543a14ad99973f9b5f05be6",
+    "url": "/static/media/parallax-1.ec79523b.jpg"
+  },
+  {
+    "revision": "9f05590bc538ccc02dabca1e4420cb86",
+    "url": "/static/media/parallax-2.9f05590b.jpg"
+  },
+  {
+    "revision": "40b5c0522304acc161f7db845dd66cd3",
+    "url": "/static/media/social-linkedin-circle-512.40b5c052.png"
+  }
+]);
