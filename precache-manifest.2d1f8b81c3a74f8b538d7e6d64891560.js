07977059abdb6a724ebbb69a2c0f1bdf8acc4601
Mat Wilmot <matwilmot90@gmail.com>
2020-08-24 15:48:08 -0700

    Updates

---
+++ precache-manifest.2d1f8b81c3a74f8b538d7e6d64891560.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e057427c3029e7d4fb2431de2af5676",
+    "url": "/20-react-portfolio/index.html"
+  },
+  {
+    "revision": "b24f3e54701b2d542408",
+    "url": "/20-react-portfolio/static/css/main.85ae41ea.chunk.css"
+  },
+  {
+    "revision": "14a73041c98f32714987",
+    "url": "/20-react-portfolio/static/js/2.79a2e5a1.chunk.js"
+  },
+  {
+    "revision": "f25112c0081db71b0e5dd2ce64e909e6",
+    "url": "/20-react-portfolio/static/js/2.79a2e5a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b24f3e54701b2d542408",
+    "url": "/20-react-portfolio/static/js/main.bed596bd.chunk.js"
+  },
+  {
+    "revision": "c4ef9c0ef4a7289fcce4",
+    "url": "/20-react-portfolio/static/js/runtime-main.66bded29.js"
+  },
+  {
+    "revision": "37a401aea4e6c05175d2f9a6d46f072d",
+    "url": "/20-react-portfolio/static/media/20190730_145147.37a401ae.jpg"
+  },
+  {
+    "revision": "ec79523b1543a14ad99973f9b5f05be6",
+    "url": "/20-react-portfolio/static/media/parallax-1.ec79523b.jpg"
+  },
+  {
+    "revision": "9f05590bc538ccc02dabca1e4420cb86",
+    "url": "/20-react-portfolio/static/media/parallax-2.9f05590b.jpg"
+  },
+  {
+    "revision": "40b5c0522304acc161f7db845dd66cd3",
+    "url": "/20-react-portfolio/static/media/social-linkedin-circle-512.40b5c052.png"
+  }
+]);
